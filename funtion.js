dc5d117dd76355b2ea0f1de8dec65b711bef4db9
Joky_Lee <1270849866@qq.com>
2018-09-06 10:36:08 +0800

    测试

--- funtion.js
+++ funtion.js
@@ -1,6 +1,5 @@
 function clickFunction()
 {
-    // document.getElementById("demo").innerHTML="哈哈";
     // alert("您点击了我的按钮哦~");
     document.getElementById("paragraph").innerHTML="你好，贵客";
     document.getElementById("myDiv").innerHTML="how are you?";
