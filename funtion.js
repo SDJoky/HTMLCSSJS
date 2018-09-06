3ac1b3d024ce84f096c3491e916fd515ff6b4a65
SDJoky <1270849866@qq.com>
2018-09-06 10:45:25 +0800

    提交再github不显示contributes的问题

--- funtion.js
+++ funtion.js
@@ -1,6 +1,5 @@
 function clickFunction()
 {
-    // alert("您点击了我的按钮哦~");
     document.getElementById("paragraph").innerHTML="你好，贵客";
     document.getElementById("myDiv").innerHTML="how are you?";
 }
