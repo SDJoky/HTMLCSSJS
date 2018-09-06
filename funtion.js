958709bd9d57a5baf889f20173872a245f41f463
SDJoky <1270849866@qq.com>
2018-09-06 10:42:59 +0800

    no message

--- funtion.js
+++ funtion.js
@@ -1,5 +1,6 @@
 function clickFunction()
 {
+    // alert("您点击了我的按钮哦~");
     document.getElementById("paragraph").innerHTML="你好，贵客";
     document.getElementById("myDiv").innerHTML="how are you?";
 }
