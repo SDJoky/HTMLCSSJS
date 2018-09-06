b8707738790164b3e33c2458f3dae60d94648dc8
Joky_Lee <1360027335@qq.com>
2018-09-06 10:27:26 +0800

    js变量

--- funtion.js
+++ funtion.js
@@ -1,5 +1,7 @@
 function clickFunction()
 {
-    // document.getElementById("demo").innerHTML="00000";
-    alert("您点击了我的按钮哦~");
+    // document.getElementById("demo").innerHTML="哈哈";
+    // alert("您点击了我的按钮哦~");
+    document.getElementById("paragraph").innerHTML="你好，贵客";
+    document.getElementById("myDiv").innerHTML="how are you?";
 }
